6b65c1dfc05a85883e566556ebb8ec2be333e081
jiang-shu-yi <3a932083@gm.student.ncut.edu.tw>
2023-02-12 22:20:33 +0800

    建立member、admin(-mcf)，user資料表新增type欄位

---
+++ database/migrations/2023_02_12_141047_create_admins_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->id();//編號
+            $table->unsignedBigInteger(' user_id '); //使用者編號
+            $table->foreign('user _id')->references('id')->on('users');
+            $table->string('position',255);//職位
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+};
