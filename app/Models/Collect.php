8aeeab2357e139565f8710e12f38143b25fe1676
jiang-shu-yi <3a932083@gm.student.ncut.edu.tw>
2023-02-22 15:06:17 +0800

    修改會員、食譜及收藏的model關聯

--- app/Models/Collect.php
+++ app/Models/Collect.php
@@ -14,4 +14,14 @@ class Collect extends Model
         'recipe_id',
     ];
 
+    //一項收藏食譜只屬於一個食譜(一對一)
+    public function recipe(){
+        return $this->belongsTo(Recipe::class);
+    }
+
+    //一項收藏食譜只能指定一個會員(一對多)
+    public function member(){
+        return $this->belongsTo(Member::class);
+    }
+
 }
--- app/Models/Member.php
+++ app/Models/Member.php
@@ -33,16 +33,14 @@ class Member extends Model
         return $this->hasMany(Card::class);
     }
 
+    //一個會員能收藏多個食譜
+    public function collects(){
+        return $this->hasMany(Collect::class);
+    }
+
     //一個會員可留下多個留言(一對多)
     public function comment(){
         return $this->hasMany(Comment::class);
-    }
-
-    //每個會員可能收藏多個食譜
-    public function recipe(){
-        return $this->belongsToMany(Recipe::class,'collects')
-                    ->withPivot('member_id','recipe_id')//pivot屬性代表中介表的模型(此為collects)，可像其它的 Eloquent 模型一樣被使用
-                    ->withTimestamps();//自動維護 created_at 和 updated_at 時間戳記
     }
 
     private function items(){
