26618d89cf67d1f0603f23ffddaf0fcb6e668d65
Chen Yi-Hsuan <s3a932087@student.ncut.edu.tw>
2023-02-22 18:48:08 +0800

    加入jzonta/faker-restaurant & products table 資料填充(50%)

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use App\Models\Product;
+use Faker\Factory;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,29 +18,34 @@ class CategorySeeder extends Seeder
     public function run()
     {
         Category::truncate();//重制資料表
+        Product::truncate();//重制資料表
+        $faker=Factory::create();
+        $faker->addProvider(new \FakerRestaurant\Provider\en_US\Restaurant($faker));
+
 
         Category::factory([
             'name'=>'肉類'
         ])->has(Category::factory(
-            ['name'=>'牛肉'],
-        ))->create();
+            [['name'=>'牛肉']]
+        ))->has(Product::factory(5,['name'=>$faker->meatname()]))->create();
 
         Category::factory([
             'name'=>'蔬菜類'
         ])->has(Category::factory(
             ['name'=>'菠菜'],
-        ))->create();
+        ))->has(Product::factory(5,['name'=>$faker->vegetableName()]))->create();
 
         Category::factory([
             'name'=>'水果類'
         ])->has(Category::factory(
             ['name'=>'蘋果'],
-        ))->create();
+        ))->has(Product::factory(5,['name'=>$faker->fruitName()]))->create();
 
         Category::factory([
-            'name'=>'菇類'
+            'name'=>'醬類'
         ])->has(Category::factory(
-            ['name'=>'香菇'],
-        ))->create();
+            ['name'=>'醬油'],
+        ))->has(Product::factory(5,['name'=>$faker->sauceName()]))->create();
+
     }
 }
