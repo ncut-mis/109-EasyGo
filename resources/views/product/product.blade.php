763b416d26f3f332d59ec861d12864103d0eb221
Yang shu mei <pbs71129@gmail.com>
2023-04-24 23:50:34 +0800

    賣場商品能點進商品詳細頁面

--- resources/views/product/product.blade.php
+++ resources/views/product/product.blade.php
@@ -16,14 +16,63 @@
     </div>
 
     <!--內容--->
+{{--    <div class=" px-lg-5" id="nav-tabContent">--}}
+{{--        <!--陣列內有幾筆資料就會重複執行幾次-->--}}
+{{--        <div class="tab-pane fade show active " id="nav-new" role="tabpanel" aria-labelledby="nav-new-tab">--}}
+{{--            <section class="pt-5">--}}
+{{--                <div class="container px-4 px-lg-5 mt-5">--}}
+{{--                    <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">--}}
+{{--                    <!-- Page Features-->--}}
+{{--                        @foreach($products as $product)--}}
+{{--                            <form action="{{route('members.cart_items.store')}}?pid={{$product->id}}" method="POST">--}}
+{{--                                @csrf--}}
+{{--                                @method('POST')--}}
+{{--                            <div class="col-lg-6 col-xxl-4 mb-5 pt-5">--}}
+{{--                                <div class="card bg-light border-0 h-100 ">--}}
+{{--                                    <!--圖片-->--}}
+{{--                                    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">--}}
+{{--                                        <div class="carousel-inner">--}}
+{{--                                            <div class="carousel-item active">--}}
+{{--                                                <img class="card-img-top" src="{{$product->product_imgs}}" alt="..." width="232px" height="232px" value="{{$product->product_imgs}}">--}}
+{{--                                            </div>--}}
+{{--                                        </div>--}}
+{{--                                    </div>--}}
 
-    <section class="py-5">
+{{--                                    <!-- Product details-->--}}
+{{--                                    <div class="card-body text-center p-lg-5  pt-lg-0 pt-5">--}}
+{{--                                         <div class="text-center">--}}
+{{--                                             <!-- Product name-->--}}
+{{--                                             <h5 class="fw-bolder" value="{{$product->name}}">{{$product->name}}</h5>--}}
+{{--                                             <!-- Product price-->--}}
+{{--                                             ${{$product->price}}--}}
+{{--                                             <a href="{{route('product.show',$product->id)}}" class="stretched-link"></a>--}}
+{{--                                         </div>--}}
+{{--                                     </div>--}}
+{{--                                    <!-- Product actions-->--}}
+{{--                                    <div class="card-footer p-2 pt-0 border-top-0 bg-transparent">--}}
+{{--                                        <div class="text-center">--}}
+{{--                                            <input type="number" name="quantity" class="form-control mb-3" value="1">--}}
+{{--                                            <button class="btn btn-outline-dark mt-auto">加入購物車</button>--}}
+{{--                                        </div>--}}
+{{--                                    </div>--}}
+{{--                                </div>--}}
+{{--                            @endforeach--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+{{--                </section>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+            <!---------------------------------------------------------------->
+        <section class="py-5">
         <div class="container px-4 px-lg-5 mt-5">
             <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
                 @foreach($products as $product)
                     <form action="{{route('members.cart_items.store')}}?pid={{$product->id}}" method="POST">
                         @csrf
                         @method('POST')
+                        <!--典籍進入詳細葉面-->
+                        <div class="pt-5">
+                        <div class="card ht border-0 h-100 ">
                         <td>   <img class="card-img-top" src="{{$product->product_imgs}}" alt="..." width="232px" height="232px" value="{{$product->product_imgs}}">
                             <!-- Product details-->
                             <div class="card-body p-4">
@@ -32,8 +81,11 @@
                                     <h5 class="fw-bolder" value="{{$product->name}}">{{$product->name}}</h5>
                                     <!-- Product price-->
                                     ${{$product->price}}
+                                    <a href="{{route('product.show',$product->id)}}" class="stretched-link"></a>
                                 </div>
                             </div>
+                        </div>
+                        </div>
                             <!-- Product actions-->
                             <div class="card-footer p-2 pt-0 border-top-0 bg-transparent">
                                 <div class="text-center">
@@ -41,6 +93,7 @@
                                     <button class="btn btn-outline-dark mt-auto">加入購物車</button>
                                 </div>
                             </div>
+                        </td>
 
                     </form>
                 @endforeach
